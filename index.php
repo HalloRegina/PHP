d63699f5e3bd8c6f503e4b3ae2f2af788094e178
HalloRegina <36815865+HalloRegina@users.noreply.github.com>
2018-02-25 17:54:37 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Index</title>
+    <h1>Index</h1>
+</head>
+<body>
+    <a href="select.php">Select</a><br>
+    <a href="insert.php">Insert</a><br>
+    <a href="delete.php">Delete</a><br>
+    <a href="search.php">Search</a><br>
+    <a href="von.php">Suchen&Updaten</a>
+</body>
+</html>
